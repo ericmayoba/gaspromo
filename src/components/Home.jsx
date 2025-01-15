c1de46bd4eb5bd79839bcf0951eb91b78913c9a4
Eric Ramirez <ericramirez@Erics-MacBook-Pro.local>
2025-01-15 19:17:20 -0400

    feat: adding source files and new component Home.jsx, Home.css andassets/logo.png

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+
+import '/src/Styles/Home.css'; 
+import logo from '../assets/logo.png';
+
+export const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="welcome-box">
+        <img 
+          src={logo}
+          alt="Coopegas Promo" 
+          className="welcome-logo"
+        />
+        <h1>Bienvenido</h1>
+        <p>
+          Bienvenido al sistema de puntos <strong>CoopegasPromo</strong> en el cual podrás registrar clientes, usuarios, sucursales y también canjear tus puntos por galones de gas.
+        </p>
+      </div>
+    </div>
+  );
+};
