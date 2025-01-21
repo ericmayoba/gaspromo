c5340ed45196842d49a6574ef68fb112557ef015
Eric Ramirez <ericmayoba@gmail.com>
2025-01-21 19:50:36 -0400

    feat: adding Visitas component with Visitas.css and Route path /visitas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Clientes from './components/Clientes'
+import { Visitas } from './components/Visitas';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Clientes" element={<Clientes />} />
+        <Route path="/Visitas" element={<Visitas />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Visitas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Visitas = () => {
+  return (
+    <div>Visitas</div>
+  )
+}
