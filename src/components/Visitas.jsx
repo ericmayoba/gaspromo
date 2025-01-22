a2785970a9c50731c5e7d7d9ea9df29da62de7e9
Eric Ramirez <ericmayoba@gmail.com>
2025-01-21 21:31:11 -0400

    feat: adding @zxing/library and code logic for barcode capture

--- src/components/Visitas.jsx
+++ src/components/Visitas.jsx
@@ -1,7 +1,97 @@
-import React from 'react'
+import { useRef, useEffect, useState } from 'react';
+import { BrowserMultiFormatReader } from '@zxing/library';
 
 export const Visitas = () => {
+    const videoRef = useRef(null);
+    const [barcode, setBarcode] = useState('');
+
+    useEffect(() => {
+        const codeReader = new BrowserMultiFormatReader();
+        const formats = [
+          'CODE_128',
+          'CODE_39',
+          'EAN_13',
+          'EAN_8',
+          'ITF',
+          'UPC_A',
+          'UPC_E',
+          'QR_CODE',
+        ];
+      
+        codeReader
+          .decodeFromVideoDevice(
+            null,
+            videoRef.current,
+            (result, err) => {
+              if (result) {
+                setBarcode(result.getText()); // Captura el código de barras
+              }
+            },
+            { formats }
+          )
+          .catch((err) => console.error(err));
+      
+        return () => {
+          codeReader.reset();
+        };
+      }, []);
+      
+
   return (
-    <div>Visitas</div>
+    <>
+        <div className="content">
+        <section className="capture-section">
+          <h3>Captura</h3>
+          <video ref={videoRef} style={{ width: '100%', height: '200px', border: '1px solid #ccc' }} />
+        {barcode && (
+          <div>
+            <strong>Código capturado:</strong> {barcode}
+          </div>
+        )}
+        </section>
+
+        <section className="datos-cliente-section">
+          <h3>Datos Cliente</h3>
+          <table className="cliente-table">
+            <thead>
+              <tr>
+                <th>Codigo</th>
+              </tr>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Vehiculo</th>
+                <th>Telefono</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <input type="text" placeholder="Nombre" />
+                </td>
+                <td>
+                  <input type="text" placeholder="Apellido" />
+                </td>
+                <td>
+                  <input type="text" placeholder="Vehiculo" />
+                </td>
+                <td>
+                  <input type="text" placeholder="Telefono" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="text" placeholder="Visitas" />
+                </td>
+                <td colSpan="3">
+                  <button className="submit-button">Submit</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </div>
+    </>
+
   )
 }
