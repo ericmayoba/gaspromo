6d7da53ba94a5ab397c1aa10a9a7424e33d66f73
Eric Ramirez <ericmayoba@gmail.com>
2025-01-23 14:29:42 -0400

    feat: Adding logic code to display a modal to create a new plant, also adding pagination to the plant table

--- src/components/Plantas.jsx
+++ src/components/Plantas.jsx
@@ -4,28 +4,36 @@ import "../Styles/Plantas.css";
 import Swal from "sweetalert2";
 
 export const Plantas = () => {
-  const [sucursales, setSucursales] = useState([]); // Estado para almacenar los datos
-  const [showModal, setShowModal] = useState(false); // Estado para controlar la visibilidad del modal
-  const [selectedPlanta, setSelectedPlanta] = useState(null); // Estado para almacenar la planta seleccionada
-  const [editData, setEditData] = useState({ nombre: '', descripcion: '' }); // Estado para editar los campos
+  const [sucursales, setSucursales] = useState([]); 
+  const [showModal, setShowModal] = useState(false); 
+  const [selectedPlanta, setSelectedPlanta] = useState(null);
+  const [editData, setEditData] = useState({ nombre: '', descripcion: '' });
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [newPlanta, setNewPlanta] = useState({ nombre: '', descripcion: '' });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1); 
+  const itemsPerPage = 5; 
 
   // Obtener datos desde el endpoint
   useEffect(() => {
     const fetchSucursales = async () => {
       try {
-        const response = await fetch('http://localhost:5244/Sucursales');
+        const response = await fetch(`http://localhost:5244/Sucursales?PageNumber=${currentPage}&PageSize=${itemsPerPage}`);
         if (!response.ok) {
           throw new Error(`Error: ${response.status}`);
         }
         const data = await response.json();
-        setSucursales(data);
+        setSucursales(data.registros); // Datos de la página actual
+        setTotalPages(data.totalRegistros); // Total de páginas desde el backend
+        console.log(sucursales);
       } catch (error) {
         console.error('Error al obtener las sucursales:', error);
       }
     };
     fetchSucursales();
-  }, []);
+  }, [currentPage]); // Reejecutar al cambiar la página actual
 
+  
   // Manejar la apertura del modal y cargar los datos de la planta seleccionada
   const handleEdit = (planta) => {
     setSelectedPlanta(planta);
@@ -93,9 +101,66 @@ export const Plantas = () => {
     }
   };
 
+  const handleShowModal = () => {
+    setNewPlanta({ nombre: '', descripcion: '' }); // Limpiar los campos del modal
+    setShowCreateModal(true); // Mostrar el modal
+  };
+
+  const handleCreateChange = (e) => {
+    const { name, value } = e.target;
+    setNewPlanta((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleCreateSave = async () => {
+    try {
+      const response = await fetch('http://localhost:5244/Sucursales', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPlanta),
+      });
+
+      if (!response.ok) {
+        Swal.fire('Error', `Error al crear planta: ${response.status}`, 'error');
+        return;
+      }
+
+      const createdPlanta = await response.json();
+      Swal.fire('Éxito', 'Planta creada con éxito', 'success');
+
+      // Agregar la nueva planta a la lista
+      setSucursales((prev) => [...prev, createdPlanta]);
+      setShowCreateModal(false); // Cerrar el modal
+    } catch (error) {
+      console.error('Error al crear la planta:', error);
+      Swal.fire('Error', 'Hubo un problema al crear la planta.', 'error');
+    }
+  };
+
+  // Funciones para cambiar de página
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
+
+
 
   return (
     <Container className="plantas-container">
+        <div className="d-flex justify-content-end align-items-center mb-3">
+      <Button variant="success" onClick={() => handleShowModal(null, "create")}>
+        Crear Planta
+      </Button>
+    </div>
       <div className="plantas-content">
         <h1>Mantenimiento de Plantas</h1>
         <Table striped bordered hover>
@@ -107,11 +172,11 @@ export const Plantas = () => {
             </tr>
           </thead>
           <tbody>
-            {sucursales.length > 0 ? (
-              sucursales.map((sucursal) => (
-                <tr key={sucursal.id}>
-                  <td>{sucursal.nombre}</td>
-                  <td>{sucursal.descripcion}</td>
+          {Array.isArray(sucursales) && sucursales.length > 0 ? (
+            sucursales.map((sucursal) => (
+              <tr key={sucursal.id}>
+                <td>{sucursal.nombre}</td>
+                <td>{sucursal.descripcion}</td>
                   <td>
                     <Button
                       variant="warning"
@@ -143,8 +208,7 @@ export const Plantas = () => {
                       >
                         Eliminar
                       </Button>
-
-                  </td>
+                    </td>
                 </tr>
               ))
             ) : (
@@ -156,8 +220,24 @@ export const Plantas = () => {
             )}
           </tbody>
         </Table>
+        <div className="d-flex justify-content-center">
+          <Button
+            variant="success"
+            disabled={currentPage === 1}
+            onClick={handlePreviousPage}
+          >
+            Anterior
+          </Button>
+          <span>Página {currentPage} de {totalPages}</span>
+          <Button
+            variant="success"
+            disabled={currentPage === totalPages}
+            onClick={handleNextPage}
+          >
+            Siguiente
+          </Button>
+        </div>      
       </div>
-
       {/* Modal para editar */}
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
@@ -195,6 +275,46 @@ export const Plantas = () => {
           </Button>
         </Modal.Footer>
       </Modal>
+
+      {/* Modal para crear */}
+    <Modal show={showCreateModal} onHide={() => setShowCreateModal(false)}>
+    <Modal.Header closeButton>
+      <Modal.Title>Crear Planta</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Form>
+        <Form.Group controlId="formPlantaNombre" className="mb-3">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control
+            type="text"
+            name="nombre"
+            value={newPlanta.nombre}
+            onChange={handleCreateChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formPlantaDescripcion" className="mb-3">
+          <Form.Label>Descripción</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            name="descripcion"
+            value={newPlanta.descripcion}
+            onChange={handleCreateChange}
+          />
+        </Form.Group>
+      </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={() => setShowCreateModal(false)}>
+        Cancelar
+      </Button>
+      <Button variant="success" onClick={handleCreateSave}>
+        Guardar
+      </Button>
+    </Modal.Footer>
+  </Modal>
     </Container>
   );
 };
+
+
